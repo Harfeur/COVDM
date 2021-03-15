ad4b01f49626eaac55125c706ba3fa5e51d84af5
Llaplace <lisa.laplace12@gmail.com>
2021-03-15 16:07:14 +0100

    Marker/popup affiché + image

--- public/js/carte.js
+++ public/js/carte.js
@@ -1,6 +1,5 @@
-
 // Ajustation taille carte en fonction de l'écran
-$("#mapid").height(((window.innerHeight)-20));
+$("#mapid").height(((window.innerHeight) - 20));
 
 //Création de la map
 var mymap = L.map('mapid').setView([46.3630104, 2.9846608], 6);
@@ -15,22 +14,27 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(mymap);
 
 //Création groupe maker
-var markersCluster = new L.MarkerClusterGroup();
+// var markersCluster = new L.MarkerClusterGroup();
 
 //Ajouter l'appel à la BDD
 
-{
-    var popup = obj.rs+
-            '<br>'+obj.adresse+
-            '<br>'+obj.horaire+
-            '<br>+obj.do_prel'+' '+obj.antigenic+' '+obj.chek_rdv+
+$.get("/sitesPrelevements").done(data => {
+    data.forEach(obj => {
+
+        var popup = obj.rs +
+            '<br>' + obj.adresse +
+            '<br>' + obj.horaire +
+            '<br>' + obj.do_prel + ' ' + obj.do_antigenic + ' ' + obj.check_rdv +
+            //_id pour identification page formul
             '<a href="#"> + </a>';
 
-    //Création du marker et de son groupe
-    var marker = L.marker([obj.lat, obj.lng], {icon: myIcon});
-    marker.bindPopup(popup)
-    markersCluster.addLayer(marker);
-}
+        //Création du marker et de son groupe
+        var marker = L.marker([obj.latitude, obj.longitude]);
+        marker.bindPopup(popup)
+        marker.addTo(mymap)
+        // markersCluster.addLayer(marker);
 
-map.addLayer(markersCluster);
-
+    });
+    
+    //map.addLayer(markersCluster);
+})
