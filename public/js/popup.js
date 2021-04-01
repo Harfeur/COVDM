ad29c49830eccde8c863fd1e0dfb4119bddde1fa
SDailhau <sandradailhau@orange.fr>
2021-04-01 15:53:26 +0200

    liaison popup

--- public/js/popup.js
+++ public/js/popup.js
@@ -2,35 +2,14 @@ new Vue({
     el: '#app',
     vuetify: new Vuetify(),
     data: () => ({
-        timeOuverture: '10:15',
-        timeFermeture: '10:15',
+        time: horaire,
         show: false,
         reveal: false,
         e1: 1,
         rating: 3,
         duree: 0,
         isActive: false,
-        items: [{
-            header: 'Commentaires :'
-        },
-            {
-                subtitle: `I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`
-            },
-            {
-                divider: true,
-                inset: true
-            },
-            {
-                subtitle: `Wish I could come, but I'm out of town this weekend.`
-            },
-            {
-                divider: true,
-                inset: true
-            },
-            {
-                subtitle: 'Do you have Paris recommendations? Have you ever been?'
-            },
-        ],
+        items: avis,
         jour: [{
             title: 'Lundi'
         },
@@ -55,13 +34,13 @@ new Vue({
         ],
         model: 1,
         dialog: false,
-        placement: 0,
         com: "",
         prenom: "",
         nom: "",
         email: "",
         alerte:false,
         alerte2:false,
+        placement: 0,
     }),
     methods: {
         ouvreCom() {
@@ -110,8 +89,21 @@ new Vue({
             console.log(this.rating);
             console.log(this.com);
             this.e1 = 5;
+        },
+        getFloatDecimalPortion(x) {
+            
+            return },
+        getHoraireToString(x) {
+            var heure = parseInt(x).toString();
+            x = Math.abs(parseFloat(x));
+            let n = parseInt(x);
+            let dec = (parseInt(parseFloat((Number((x - n).toFixed(Math.abs((""+x).length - (""+n).length - 1)))).toFixed(2)) * 100)).toString().padStart(2, "0");
+            var minute = dec.toString();
+            return heure+":"+minute;
+        
         }
-    }
+    },
+    
 })
 function checkEmail(email) {
     var re = /^(([^<>()[\\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
