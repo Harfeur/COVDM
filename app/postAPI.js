b6c5ed0b319bf70ccd60fa90297987cce01da6fb
Harfeur <maxou3153@gmail.com>
2021-04-08 13:26:52 +0200

    Requete pour ajouter un commentaire

--- app/postAPI.js
+++ app/postAPI.js
@@ -9,6 +9,31 @@ const {Db} = require('mongodb');
 
 module.exports = function (app, db, dirname) {
 
-
+    app.post('/ajoutCommentaire', (req, res) => {
+        if (req.body.nom && req.body.email && req.body.id && req.body.note) {
+            const filter = {"_id": id};
+            if (req.body.attente !== undefined) {
+                db.updateOne(filter, {$push: {"stats.attente": req.body.attente}});
+            }
+            if (req.body.bonDeroulement !== undefined) {
+                if (req.body.bonDeroulement)
+                    db.updateOne(filter, {$inc: {"stats.bonDeroulement.oui": 1}});
+                else
+                    db.updateOne(filter, {$inc: {"stats.bonDeroulement.non": 1}});
+            }
+            db.updateOne(filter, {
+                $push: {
+                    "avis": {
+                        "nom": req.body.nom,
+                        "email": req.body.email,
+                        "message": req.body.message === "" ? "Pas de commentaire" : req.body.message,
+                        "note": req.body.note
+                    }
+                }
+            })
+        } else {
+            res.status(500);
+        }
+    });
 
 }
