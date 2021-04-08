a9d952fa0dafbe7767cd8412738a16f8d0234045
Llaplace <lisa.laplace12@gmail.com>
2021-04-08 13:28:50 +0200

    Stats par region

--- public/js/stats.js
+++ public/js/stats.js
@@ -4,8 +4,14 @@ var couleur = [];
 var total = 0;
 var nbAvis = [];
 var avis = [];
-//-------------------------------------------------------- Région ----------------------------------------------------------------------------------------
 
+
+
+
+const urlParams = new URLSearchParams(window.location.search);
+let id_region = urlParams.get('id_region');
+
+//-------------------------------------------------------- Région ----------------------------------------------------------------------------------------
 //Données
 $.get("/regions").done(dataR => {
 
@@ -24,8 +30,9 @@ $.get("/regions").done(dataR => {
 
 
     //Données
-    $.get("/sitesPrelevements").done(dataP => {
-
+    $.get("/sitesPrelevements",{id_region :id_region}).done(dataP => {
+        
+        
         //maj cluster
         dataP.forEach(obj => {
             cluster[obj.adresse.codeRegion].y += 1
@@ -42,7 +49,7 @@ $.get("/regions").done(dataR => {
             
         });
 
-        console.log(nbAvis);
+        //console.log(nbAvis);
         //mis en pourcentage + ajout dans liste pour les datas chart pie
         for (const [key, value] of Object.entries(cluster)) {
             cluster[key].y = ((value.y * 100) / total).toFixed(1)
@@ -56,8 +63,14 @@ $.get("/regions").done(dataR => {
         //couleur
         CanvasJS.addColorSet("couleurRegion", couleur);
 
-        //création chart pie nb site par region 
-        var chart = new CanvasJS.Chart("chartContainerNB", {
+
+
+
+
+        //affichage chart pie
+        if(!id_region){    
+         //nb site par region 
+        var chart = new CanvasJS.Chart("chartContainer", {
             colorSet: "couleurRegion",
             exportEnabled: true,
             animationEnabled: true,
@@ -75,27 +88,29 @@ $.get("/regions").done(dataR => {
                 dataPoints: siteP
             }]
         });
-
-        var chartAvis = new CanvasJS.Chart("chartContainer", {
-            animationEnabled: true,
-            exportEnabled: true,
-            theme: "light1", // "light1", "light2", "dark1", "dark2"
-            title:{
-                text: "Nombre avis par site dans toute la France"
-            },
-            data: [{
-                type: "column", //change type to bar, line, area, pie, etc
-                //indexLabel: "{y}", //Shows y value on all Data Points
-                indexLabelFontColor: "#5A5757",
-                  indexLabelFontSize: 16,
-                indexLabelPlacement: "outside",
-                toolTipContent: "<b>{label}</b><br>Ville: {ville}<br><br>Nombre d'avis : {y}",
-                dataPoints: nbAvis
-            }]
-        });
-        //affichage chart pie
-        chart.render();
-        chartAvis.render();
+        
+        chart.render();  
+        }else{    
+            //Pour chaque région nb avis
+            var chartAvis = new CanvasJS.Chart("chartContainer", {
+                animationEnabled: true,
+                exportEnabled: true,
+                theme: "light1", // "light1", "light2", "dark1", "dark2"
+                title:{
+                    text: "Nombre d'avis par site en "+cluster[id_region].name
+                },
+                data: [{
+                    type: "column", //change type to bar, line, area, pie, etc
+                    //indexLabel: "{y}", //Shows y value on all Data Points
+                    indexLabelFontColor: "#5A5757",
+                      indexLabelFontSize: 16,
+                    indexLabelPlacement: "outside",
+                    toolTipContent: "<b>{label}</b><br>Ville: {ville}<br><br>Nombre d'avis : {y}",
+                    dataPoints: nbAvis
+                }]
+            }); 
+            chartAvis.render();
+        }
 
     });
 
