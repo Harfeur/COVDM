8978d45ac93ed58ddd767325b41997a2db7d060d
SDailhau <sandradailhau@orange.fr>
2021-04-08 15:38:10 +0200

    Bug mail résolu

--- public/js/popup.js
+++ public/js/popup.js
@@ -111,7 +111,7 @@ new Vue({
     },
     
 })
-function checkEmail(email) {
-    var re = /^(([^<>()[\\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+function checkEmail(inputText) {
+    var expressionReguliere = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+    return expressionReguliere.test(inputText)
 }
