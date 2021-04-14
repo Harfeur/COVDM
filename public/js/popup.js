de80285c5ea0f8a487c77db0fed4452626aae1d4
SDailhau <sandradailhau@orange.fr>
2021-04-14 15:45:38 +0200

    Reglage bug

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,223 +1,252 @@
-new Vue({
-    el: '#app',
-    vuetify: new Vuetify(),
-    data: () => ({
-        time: horaire,
-        show: false,
-        reveal: false,
-        id: id,
-        e1: 1,
-        rating: 3,
-        duree: null,
-        isActive: false,
-        items: avis,
-        jour: [{
-                title: 'Lundi'
-            },
-            {
-                title: 'Mardi'
-            },
-            {
-                title: 'Mercredi'
-            },
-            {
-                title: 'Jeudi'
-            },
-            {
-                title: 'Vendredi'
-            },
-            {
-                title: 'Samedi'
-            },
-            {
-                title: 'Dimanche'
-            },
-        ],
-        model: 1,
-        dialog: false,
-        dialog1: false,
-        dialog2: false,
-        sheet: false,
-        com: null,
-        prenom: "",
-        nom: "",
-        email: "",
-        alerte: false,
-        alerte2: false,
-        placement: 0,
-        heureO: 1,
-        heureF: 1,
-        der: null,
-        pb1: true,
-    }),
-    methods: {
-        ouvreCom() {
-            var expansion = document.getElementById('exp');
-            this.reveal = true;
-            expansion.style.display = "none";
-        },
-        fermeCom() {
-            var expansion = document.getElementById('exp');
-            this.reveal = false;
-            expansion.style.display = "block";
-        },
-        scrollMoiStp() {
-            setTimeout(scroll, 200);
-        },
-        liste(index) {
-            this.placement = index;
-        },
-        partageExp() {
-            this.show=!this.show;
-            setTimeout(scroll, 400);
-        },
-        presenter() {
-            if (this.prenom == "" || this.nom == "" || this.email == "") {
-                this.alerte = true;
-            } else {
-                if (checkEmail(this.email)) {
-                    console.log(this.prenom + ' ' + this.nom);
-                    console.log(this.email);
-                    this.dialog = false;
-                    this.e1 = 2;
-                    setTimeout(scroll, 400);
-                } else {
-                    this.alerte2 = true;
-                }
-            }
-        },
-        deroulement(choix) {
-            if (choix == 1) {
-                this.der = true;
-            } else {
-                this.der = false;
-            }
-            this.e1 = 3;
-            setTimeout(scroll, 400);
-        },
-        attente() {
-            console.log(this.duree);
 
-            this.e1 = 4;
-            setTimeout(scroll, 400);
-        },
-        eval() {
-            console.log(this.rating);
-            console.log(this.com);
-            this.e1 = 5;
 
-            fetch('/ajoutCommentaire', {
-                method: 'POST',
-                body: JSON.stringify({
-                    nom: this.prenom + ' ' + this.nom,
-                    email: this.email,
-                    id: this.id,
-                    note: this.rating,
-                    attente: this.duree,
-                    bonDeroulement: this.der,
-                    message: this.com
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-        },
-        getHoraireToString(x) {
-            var heure = parseInt(x).toString();
-            x = Math.abs(parseFloat(x));
-            let n = parseInt(x);
-            let dec = (parseInt(parseFloat((Number((x - n).toFixed(Math.abs(("" + x).length - ("" + n).length - 1)))).toFixed(2)) * 100)).toString().padStart(2, "0");
-            var minute = dec.toString();
-            return heure + ":" + minute;
-        },
-        getStringToHoraire(x){
-            var tab = x.split(':');
-            var hh = parseInt(tab[0]);
-            var mm = parseInt(tab[1])/100;
-            return hh+mm;
-
-        },
-        majHoraire(){
-            if (horaire[(this.jour[this.placement].title).toLowerCase()].length != 0) {
-                this.heureO=this.getHoraireToString(this.time[(this.jour[this.placement].title).toLowerCase()][0][0]);
-                this.heureF=this.getHoraireToString(this.time[(this.jour[this.placement].title).toLowerCase()][0][1]);
-            }
-            console.log(this.heureO);
-        },
-        ouvreHoraire1(){
-            this.majHoraire();
-            this.dialog1=true;
-        },
-        ouvreHoraire2(){
-            this.majHoraire();
-            this.dialog2=true;
-        },
-        modifHoraireOuverture() {
-            if (horaire[(this.jour[this.placement].title).toLowerCase()].length != 0) {
-                var heureOuverture = this.getStringToHoraire(this.heureO);
-                if(heureOuverture<horaire[(this.jour[this.placement].title).toLowerCase()][0][1]){
-                    this.dialog1=false;
-                    fetch('/majHoraire', {
-                        method: 'POST',
-                        body: JSON.stringify({
-                            id: this.id,
-                            jour: (this.jour[this.placement].title).toLowerCase(),
-                            heureO: heureOuverture,
-                            heureF: horaire[(this.jour[this.placement].title).toLowerCase()][0][1]
-                        }),
-                        headers: {
-                            'Content-Type': 'application/json'
-                        }
-                    })
-                    this.majHoraire;
-                }
-                else {
-                    this.pb1=true;
-                    this.sheet=true;
-                }
-
-            }
-        },
-        modifHoraireFermeture() {
-            if (horaire[(this.jour[this.placement].title).toLowerCase()].length != 0) {
-                var heureFermeture = this.getStringToHoraire(this.heureF);
-                console.log(heureFermeture>horaire[(this.jour[this.placement].title).toLowerCase()][0][0])
-                if(heureFermeture>horaire[(this.jour[this.placement].title).toLowerCase()][0][0]){
-                    this.dialog2 = false;
-                    fetch('/majHoraire', {
-                        method: 'POST',
-                        body: JSON.stringify({
-                            id: this.id,
-                            jour: (this.jour[this.placement].title).toLowerCase(),
-                            heureO: horaire[(this.jour[this.placement].title).toLowerCase()][0][0],
-                            heureF: heureFermeture
-                        }),
-                        headers: {
-                            'Content-Type': 'application/json'
-                        }
-                    })
-                    this.majHoraire;
-                }
-                else {
-                    this.pb1=false;
-                    this.sheet=true;
-                }
-
-            }
+window.addEventListener("DOMContentLoaded", (event) => {
+    
+    var el = document.getElementById('footer');
+    function majHauteur(x){
+        switch(x){
+            case 1:el.style.setProperty("height", 150+'px');break;
+            case 2:el.style.setProperty("height", 235+'px');break;
+            default:el.style.setProperty("height", 295+'px');break;
         }
-    },
+    }
+    
+    new Vue({
+        el: '#app',
+        vuetify: new Vuetify(),
+        data: () => ({
+            time: horaire,
+            show: false,
+            reveal: false,
+            id: id,
+            e1: 1,
+            rating: 3,
+            duree: null,
+            isActive: false,
+            items: avis,
+            jour: [{
+                    title: 'Lundi'
+                },
+                {
+                    title: 'Mardi'
+                },
+                {
+                    title: 'Mercredi'
+                },
+                {
+                    title: 'Jeudi'
+                },
+                {
+                    title: 'Vendredi'
+                },
+                {
+                    title: 'Samedi'
+                },
+                {
+                    title: 'Dimanche'
+                },
+            ],
+            model: 1,
+            dialog: false,
+            dialog1: false,
+            dialog2: false,
+            sheet: false,
+            com: null,
+            prenom: "",
+            nom: "",
+            email: "",
+            alerte: false,
+            alerte2: false,
+            placement: 0,
+            heureO: 1,
+            heureF: 1,
+            der: null,
+            pb1: true,
+            img: true,
+            tabs: null,
+        }),
+        methods: {
+            ouvreCom() {
+                var expansion = document.getElementById('exp');
+                this.reveal = true;
+                expansion.style.display = "none";
+            },
+            fermeCom() {
+                var expansion = document.getElementById('exp');
+                this.reveal = false;
+                expansion.style.display = "block";
+            },
+            tailleCom(){
+                switch(avis.length){
+                    case 1: majHauteur(1); break;
+                    case 2: majHauteur(2); break;
+                    default: majHauteur(3); break;
+                }
+                return true;
+            },
+            scrollMoiStp() {
+                this.img=!this.img;
+                setTimeout(scroll, 200);
+            },
+            liste(index) {
+                this.placement = index;
+            },
+            partageExp() {
+                this.show=!this.show;
+                setTimeout(scroll, 400);
+            },
+            presenter() {
+                if (this.prenom == "" || this.nom == "" || this.email == "") {
+                    this.alerte = true;
+                } else {
+                    if (checkEmail(this.email)) {
+                        this.dialog = false;
+                        this.e1 = 2;
+                        setTimeout(scroll, 400);
+                    } else {
+                        this.alerte2 = true;
+                    }
+                }
+            },
+            deroulement(choix) {
+                if (choix == 1) {
+                    this.der = true;
+                } else {
+                    this.der = false;
+                }
+                this.e1 = 3;
+                setTimeout(scroll, 400);
+            },
+            attente() {
+                this.e1 = 4;
+                setTimeout(scroll, 400);
+            },
+            eval() {
+                this.e1 = 5;
 
-})
+                fetch('/ajoutCommentaire', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        nom: this.prenom + ' ' + this.nom,
+                        email: this.email,
+                        id: this.id,
+                        note: this.rating,
+                        attente: this.duree,
+                        bonDeroulement: this.der,
+                        message: this.com
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                window.location.reload();
+            },
+            getHoraireToString(x) {
+                var heure = parseInt(x).toString();
+                x = Math.abs(parseFloat(x));
+                let n = parseInt(x);
+                let dec = (parseInt(parseFloat((Number((x - n).toFixed(Math.abs(("" + x).length - ("" + n).length - 1)))).toFixed(2)) * 100)).toString().padStart(2, "0");
+                var minute = dec.toString();
+                return heure + ":" + minute;
+            },
+            getStringToHoraire(x){
+                var tab = x.split(':');
+                var hh = parseInt(tab[0]);
+                var mm = parseInt(tab[1])/100;
+                return hh+mm;
 
-function checkEmail(inputText) {
-    var expressionReguliere = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
-    return expressionReguliere.test(inputText)
-}
+            },
+            majHoraire(){
+                if (horaire[(this.jour[this.placement].title).toLowerCase()].length != 0) {
+                    this.heureO=this.getHoraireToString(this.time[(this.jour[this.placement].title).toLowerCase()][0][0]);
+                    this.heureF=this.getHoraireToString(this.time[(this.jour[this.placement].title).toLowerCase()][0][1]);
+                    this.tabs=null;
+                    return true
+                }
+                this.tabs='one';
+                return false
+            },
+            ouvreHoraire1(){
+                this.dialog1=true;
+            },
+            ouvreHoraire2(){
+                this.dialog2=true;
+            },
+            modifHoraireOuverture() {
+                if (horaire[(this.jour[this.placement].title).toLowerCase()].length != 0) {
+                    var heureOuverture = this.getStringToHoraire(this.heureO);
+                    if(heureOuverture<horaire[(this.jour[this.placement].title).toLowerCase()][0][1]){
+                        this.dialog1=false;
+                        fetch('/majHoraire', {
+                            method: 'POST',
+                            body: JSON.stringify({
+                                id: this.id,
+                                jour: (this.jour[this.placement].title).toLowerCase(),
+                                heureO: heureOuverture,
+                                heureF: horaire[(this.jour[this.placement].title).toLowerCase()][0][1]
+                            }),
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+                        })
+                        window.location.reload();
+                    }
+                    else {
+                        this.pb1=true;
+                        this.sheet=true;
+                    }
 
-function scroll() {
-    window.scrollTo({
-        top: document.body.scrollHeight,
-        left: 0,
-        behavior: 'smooth'
-    });
-}
+                }
+            },
+            modifHoraireFermeture() {
+                if (horaire[(this.jour[this.placement].title).toLowerCase()].length != 0) {
+                    var heureFermeture = this.getStringToHoraire(this.heureF);
+                    console.log(heureFermeture>horaire[(this.jour[this.placement].title).toLowerCase()][0][0])
+                    if(heureFermeture>horaire[(this.jour[this.placement].title).toLowerCase()][0][0]){
+                        this.dialog2 = false;
+                        fetch('/majHoraire', {
+                            method: 'POST',
+                            body: JSON.stringify({
+                                id: this.id,
+                                jour: (this.jour[this.placement].title).toLowerCase(),
+                                heureO: horaire[(this.jour[this.placement].title).toLowerCase()][0][0],
+                                heureF: heureFermeture
+                            }),
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+                        })
+                        window.location.reload();
+                    }
+                    else {
+                        this.pb1=false;
+                        this.sheet=true;
+                    }
+
+                }
+            }
+        },
+        computed: {
+            activeFab () {
+              switch (this.tabs) {
+                case 'one': return { disabled: ''}
+                default: return {}
+              }
+            },
+        },
+
+    })
+
+    function checkEmail(inputText) {
+        var expressionReguliere = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+        return expressionReguliere.test(inputText)
+    }
+
+    function scroll() {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+});
